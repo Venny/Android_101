8d5b73e6d3f18bed1989f5e467db74edac395643
Veneta Dimitova <dim.veneta@gmail.com>
2016-09-08 23:46:15 +0300

    23. Returning the Details Activity /The fragment will be implemented via different  tabs views/.

---
+++ InspiredVideo/app/src/main/java/com/example/inspired/inspiredvideo/view/activities/MovieItemDetailsActivity.java
@@ -0,0 +1,82 @@
+package com.example.inspired.inspiredvideo.view.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.inspired.inspiredvideo.R;
+import com.example.inspired.inspiredvideo.model.Movie2;
+import com.example.inspired.inspiredvideo.view.adapter.MovieViewHolder;
+
+/**
+ * Created by inspired on 08.09.16.
+ */
+
+public class MovieItemDetailsActivity extends AppCompatActivity {
+    private Movie2 mMovieItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        mMovieItem = (Movie2) intent.getSerializableExtra("item");
+        setContentView(R.layout.activity_movie_item_details);
+        View view = this.getWindow().getDecorView().findViewById(android.R.id.content);
+        MovieViewHolder mVideoHolder = new MovieViewHolder(view);
+
+       /* mVideoHolder.getmImageView().setImageResource(mMovieItem.getImageRes());
+        mVideoHolder.getmNameView().setText(mMovieItem.getName());
+        mVideoHolder.getmTextDescription().setText(mMovieItem.getDescription());*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_menu_movie_item_details, menu);
+        MenuItem favouriteBtn = menu.findItem(R.id.favourite_btn);
+        if(mMovieItem.isFavourite()) {
+            favouriteBtn.setIcon(R.drawable.ic_favorite_white_24dp);
+        }
+        favouriteBtn.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(mMovieItem.isFavourite()){
+                    // Update item.
+                   // mMovieItem.setIsFavourite(false);
+                    item.setIcon(R.drawable.ic_favorite_border_white_24dp);
+                } else{
+                   // updateGrid(mMovieItem);
+                    item.setIcon(R.drawable.ic_favorite_white_24dp);
+                }
+                return true;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+   /* private boolean updateGrid(VideoItem mVideoItem){
+        mVideoItem.setIsFavourite(true);
+        Context.mData.set(mVideoItem.getPosition(), mVideoItem);
+        Context.mCurrentData.set(mVideoItem.getCurrentPosition(), mVideoItem);
+        return true;
+    }*/
+}

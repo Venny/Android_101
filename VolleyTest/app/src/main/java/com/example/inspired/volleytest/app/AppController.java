26d2a746523c9657d916c02900bb24df45572aba
Veneta Dimitova <dim.veneta@gmail.com>
2016-09-14 22:32:44 +0300

    26. Starting some simple form implementation

--- VolleyTest/app/src/main/java/com/example/inspired/volleytest/app/AppController.java
+++ VolleyTest/app/src/main/java/com/example/inspired/volleytest/app/AppController.java
@@ -1,7 +1,49 @@
 package com.example.inspired.volleytest.app;
 
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NoCache;
+import com.android.volley.toolbox.Volley;
+
 /**
  * Created by inspired on 15.07.16.
  */
-public class AppController {
+public class AppController extends Application {
+    private RequestQueue mRequestQueue;
+    private RequestQueue mNoCacheRequestQueue;
+    private ImageLoader mImageLoader;
+    private static  AppController mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getmRequestQueue(boolean shouldCache){
+        if(!shouldCache){
+            return getmRequestQueue();
+        } else {
+            if(mNoCacheRequestQueue == null) {
+                mNoCacheRequestQueue = new RequestQueue(new NoCache(), new BasicNetwork(new HurlStack()));
+            }
+        }
+        return mNoCacheRequestQueue;
+    }
+
+    public RequestQueue getmRequestQueue(){
+        if(mRequestQueue == null){
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
 }
